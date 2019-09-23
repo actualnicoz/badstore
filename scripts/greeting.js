d28883feefc193a9e81c0834612e525e3913d242
Nic <actualnicoz@protonmail.com>
2019-09-22 20:35:56 -0400

    reformatting

---
+++ scripts/greeting.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Friend!'
+    }
+})
+/*
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+*/
